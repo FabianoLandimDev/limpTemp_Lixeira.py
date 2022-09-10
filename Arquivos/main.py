bc8ed1f453e9f1a3c4f9b22c3d110b8f5477d344
Fabiano Landim <landimfabiano01@gmail.com>
2022-09-10 12:23:46 -0300

    Update main.py
    acrescentando mais duas linhas de código para sanar um problema!

--- Arquivos/main.py
+++ Arquivos/main.py
@@ -1,6 +1,7 @@
 import pyautogui as ag
 import time as t
 
+
 # Nome_do _programa: limpTemp_Lixeira.py
 #
 # site: https://github.com/FabianoLandimDev//limpTemp_Lixeira.py/blob/main/Arquivos/main.py
@@ -24,7 +25,7 @@ ag.PAUSE = 2 # 2 segundos para cada ação...
 ag.alert('Confirme a execução do programa e não mexa na máquina até o término...')
 
 
-#Abrindo o executar no Windowns e procurando pela pasta temp...
+#Inputando o comando executar no Windowns e procurando pela pasta temp...
 ag.hotkey('win', 'r')
 ag.write('temp')
 ag.press('enter')
@@ -33,9 +34,11 @@ ag.press('enter')
 ag.hotkey('ctrl', 'a')
 ag.hotkey('shift', 'del')
 ag.press('enter')
+ag.press(['right', 'right'])
+ag.press('enter')
 ag.hotkey('alt', 'F4')
 
-#Abrindo o executar no Windowns e procurando pela pasta temp...
+#Inputando o comando executar no Windowns e procurando pela pasta %temp%...
 ag.hotkey('win', 'r')
 ag.write('%temp%')
 ag.press('enter')
@@ -48,7 +51,7 @@ ag.press(['right', 'right'])
 ag.press('enter')
 ag.hotkey('alt', 'F4')
 
-#Abrindo o executar no Windowns e procurando pela pasta temp...
+#Localizando a pasta referemte à Lixeira...
 ag.hotkey('win')
 ag.write('Lixeira')
 ag.press('backspace')
